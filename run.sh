88c98c626ac2898bc38077cfbdddd4f79276de22
Colin Sauze <cos@aber.ac.uk>
2018-07-25 16:02:44 +0100

    redirecting output and fixing boat-opencpn args

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-sailsd/sailsd &
+sailsd/sailsd > /dev/null &
 sleep 1
 ./init_sails.sh
 boatd boatd.yml &
 sleep 5
 boatdctl behaviour-start example
-boatd-opencpn/boatd-opencpn &
+boatd-opencpn/boatd-opencpn 127.0.0.1:2223 &
 python recvBoatData.py
