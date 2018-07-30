c78bfb8c13d9a7469664ce09f19a264915ddb427
Colin Sauze <cos@aber.ac.uk>
2018-07-30 18:00:38 +0100

    killing off everything when we finish

--- run.sh
+++ run.sh
@@ -8,4 +8,6 @@ boatd-opencpn/boatd-opencpn 127.0.0.1:2223 10111 &
 python recvBoatData.py
 killall boatd
 killall sailsd
-killall boatd-opencpn
+kill `ps a | grep "boatd-opencpn 127.0.0.1:2223 10111" | grep -v grep | awk '{print $1}'`
+kill `ps a | grep "python simulator-behaviour/looped-waypoint-behaviour" | grep -v grep | awk '{print $1}'`
+
