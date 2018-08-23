0dcfa8e7332c376009cde62d04f600fdadb91770
Colin Sauze <cos@aber.ac.uk>
2018-08-23 11:36:50 +0100

    changing kill command to kill local version of boatd

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sleep 5
 boatdctl behaviour-start example
 boatd-opencpn/boatd-opencpn 127.0.0.1:2223 10111 &
 python recvBoatData.py
-killall boatd
+kill `ps a | grep "boatd.yml" | grep -v grep | awk '{print $1}'`
 killall sailsd
 kill `ps a | grep "boatd-opencpn 127.0.0.1:2223 10111" | grep -v grep | awk '{print $1}'`
 kill `ps a | grep "python simulator-behaviour/looped-waypoint-behaviour" | grep -v grep | awk '{print $1}'`
